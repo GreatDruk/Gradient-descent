8fe9e56b4e8ca5e01b8ff59faa000721c548498b
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-06-15 19:25:44 +0300

    Made GradientDescent

--- descents.py
+++ descents.py
@@ -52,7 +52,7 @@ class BasicDescent:
     def calculate_Log_cosh(self, y: np.ndarray, y_pred: np.ndarray) -> float:
         return np.mean(np.log(np.cosh(y - y_pred)))
     
-    def __call__(self, x: np.ndarray, y: np.ndarray) -> float:
+    def calculate_loss(self, x: np.ndarray, y: np.ndarray) -> float:
         y_pred: np.ndarray = self.predict(x)
         if self.loss_function == LossFunctions.MSE:
             return self.calculate_MSE(y, y_pred)
@@ -65,3 +65,32 @@ class BasicDescent:
         else:
             raise NotImplementedError(f"Unsupported loss: {self.loss_function}")
         
+
+class GradientDescent(BasicDescent):
+    def update_weights(self, gradient: np.ndarray) -> np.ndarray:
+        w_diff = -self.learning_rate() * gradient
+        self.w += w_diff
+        return w_diff
+
+    def calc_gradient(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        y_pred = self.predict(x)
+
+        if self.loss_function == LossFunctions.MSE:
+            return (2 * x.T @ (y_pred - y)) / len(y)
+        elif self.loss_function == LossFunctions.MAE:
+            return (x.T @ np.sign(y_pred - y)) / len(y)
+        elif self.loss_function == LossFunctions.Huber:
+            diff = y_pred - y
+            delta = 1.0
+            if np.abs(diff) < delta:
+                return (x.T @ diff) / len(y)
+            else:
+                return (x.T @ np.sign(diff)) / len(y)
+        elif self.loss_function == LossFunctions.LogCosh:
+            return (x.T @ np.tanh(y_pred - y)) / len(y)
+        else:
+            raise NotImplementedError(f"Unsupported loss: {self.loss_function}")
+
+    def step(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        gradient: np.ndarray = self.calc_gradient(x, y)
+        return self.update_weights(gradient)
